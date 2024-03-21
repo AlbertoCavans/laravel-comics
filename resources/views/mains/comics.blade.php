76f8bc3a44893b220718f623514675db0f7c0d9f
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-21 16:19:15 +0100

    Feat comics.blade.php & home.blade.php - extend GenStructure

---
+++ resources/views/mains/comics.blade.php
@@ -0,0 +1,7 @@
+@extends('GenStructure.GenStructure')
+
+@section('title', 'Comics')
+
+@section('main')
+<h1>Main-Comics</h1>
+@endsection
---
+++ resources/views/mains/home.blade.php
@@ -0,0 +1,7 @@
+@extends('GenStructure.GenStructure')
+
+@section('title', 'Home')
+
+@section('main')
+<h1>Main-Home</h1>
+@endsection
