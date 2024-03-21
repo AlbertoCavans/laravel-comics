0482e8aa9b60836716975cc4fc683be6f7328aef
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-21 16:16:43 +0100

    Feat GenStructure.blade.php - rename & manage includes+yields

--- resources/views/GenStructure/GenStructure.blade.php
+++ resources/views/GenStructure/GenStructure.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Boolcomics - @yield('title')</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -13,11 +13,26 @@
     <!-- Styles -->
     @vite('resources/js/app.js')
 
+    @yield('css')
+
 </head>
 
 <body>
 
-<h1>Laravel comics</h1>
+@include('partials.header')
+
+<main>
+@yield('main')
+</main>
+
+@include('partials.footer')
+
+
+
+
+
+
+@yield('js')
 
 </body>
 
