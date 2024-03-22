931371e22dbaed9e0607f8e9ebfaec817c74c4a8
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-22 08:27:50 +0100

    Feat footer.blade.php - first steps about details

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,16 @@
-<footer>Footer</footer>
+<footer>
+    
+<div class="container-fluid towards-payments">
+  <div class="container">
+<ul class="w-100 d-flex justify-content-around align-items-center py-3 h6">
+    <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-digital-comics.png") }}" alt=""><span></span>DIGITAL COMICS</li>
+    <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-merchandise.png") }}" alt=""><span></span>DC MERCHANDISE</li>
+    <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-subscriptions.png") }}" alt=""><span></span>SUBSCRIPTION</li>
+    <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-shop-locator.png") }}" alt=""><span></span>COMIC SHOP LOCATOR</li>
+    <li><img width="50px" class="mx-3" src="{{ url("/buy-dc-power-visa.svg") }}" alt=""><span></span>DC POWER VISA</li>
+</ul>
+  </div>
+</div>
+
+
+</footer>
