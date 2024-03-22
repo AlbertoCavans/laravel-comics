460d1b6bb4ec2d7f634ea15a8135b24ec0055754
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-22 08:27:09 +0100

    Feat comics.blade.php - details main of comics-page

--- resources/views/mains/comics.blade.php
+++ resources/views/mains/comics.blade.php
@@ -3,5 +3,23 @@
 @section('title', 'Comics')
 
 @section('main')
-<h1>Main-Comics</h1>
+<div class="container-fluid container-main">
+<div class="container py-5">
+
+    <div class="row">
+    @foreach ($comics as $comic)
+
+        <div class="col-2">
+            <img src="{{ $comic["thumb"] }}" alt="{{ $comic["title"] }}" height="300px" class="object-fit-cover w-100">
+            <h5 class="text-light">{{ $comic["series"] }}</h5>
+        </div>
+    <!-- @dump($comic) -->
+    @endforeach
+
+    </div>
+    <div class="d-flex justify-content-center">
+        <button class="button-bluewhite">LOAD MORE</button>
+    </div>
+</div>
+</div>
 @endsection
