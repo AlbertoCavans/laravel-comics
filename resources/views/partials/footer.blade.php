36d7d18c6517530a5c96aca55bc3a99a8887740f
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-22 15:01:06 +0100

    Feat footer.blade.php - footer progress

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,29 +2,84 @@
     
 <div class="container-fluid towards-payments">
   <div class="container">
-<ul class="w-100 d-flex justify-content-around align-items-center py-3 h6">
-    <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-digital-comics.png") }}" alt=""><span></span>DIGITAL COMICS</li>
-    <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-merchandise.png") }}" alt=""><span></span>DC MERCHANDISE</li>
-    <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-subscriptions.png") }}" alt=""><span></span>SUBSCRIPTION</li>
-    <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-shop-locator.png") }}" alt=""><span></span>COMIC SHOP LOCATOR</li>
-    <li><img width="50px" class="mx-3" src="{{ url("/buy-dc-power-visa.svg") }}" alt=""><span></span>DC POWER VISA</li>
-</ul>
+    <ul class="w-100 d-flex justify-content-around align-items-center py-3 h6">
+        <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-digital-comics.png") }}" alt=""><span></span>DIGITAL COMICS</li>
+        <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-merchandise.png") }}" alt=""><span></span>DC MERCHANDISE</li>
+        <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-subscriptions.png") }}" alt=""><span></span>SUBSCRIPTION</li>
+        <li><img width="50px" class="mx-3" src="{{ url("/buy-comics-shop-locator.png") }}" alt=""><span></span>COMIC SHOP LOCATOR</li>
+        <li><img width="50px" class="mx-3" src="{{ url("/buy-dc-power-visa.svg") }}" alt=""><span></span>DC POWER VISA</li>
+    </ul>
   </div>
 </div>
 
 <div class="container-fluid center-footer">
-<div class="container">
-    <ul> <h4>DC COMICS</h4>
-        <li>Characters</li>
-        <li>Comics</li>
-        <li>Movies</li>
-        <li>TV</li>
-        <li>Games</li>
-        <li>Videos</li>
-        <li>News</li>
-    </ul>
+<div class="container" >
+    <div class="row">
+         <div class="col-8" id="center-footer-container">
+
+
+         <ul> <h4>DC COMICS</h4>
+             <li>Characters</li>
+             <li>Comics</li>
+             <li>Movies</li>
+             <li>TV</li>
+             <li>Games</li>
+             <li>Videos</li>
+             <li>News</li>
+         </ul>
+     
+         <ul> <h4>SHOP</h4>
+             <li>Shop DC</li>
+             <li>Shop DC Collectibles</li>
+         </ul>
+     
+         <ul> <h4>DC</h4>
+             <li>Terms Of Use</li>
+             <li>Privacy policy (New)</li>
+             <li>Ad Choices</li>
+             <li>Advertising</li>
+             <li>Jobs</li>
+             <li>Subscriptions</li>
+             <li>Talent Workshop</li>
+             <li>CPSC Certificates</li>
+             <li>Ratings</li>
+             <li>Shop Help</li>
+             <li>Contact Us</li>
+         </ul>
+     
+         <ul> <h4>SITES</h4>
+             <li>DC</li>
+             <li>MAD Magazine</li>
+             <li>DC Kids</li>
+             <li>DC Universe</li>
+             <li>DC Power Visa</li>
+         </ul>
+   
+        </div>
+  
+        <div class="col-4">
+           <img src="{{ url("/dc-logo-bg.png") }}" alt="" width="300px">      
+        </div>
+
+    </div>
+    <div class="rights-section">
+        <p>All Site Content TM and c 2020 DC Entertainmrnt, unless otherwise 
+        <a href="#">noted here</a>. All rights reserved.
+        <a href="#">Cookie Settings</a></p>
+    </div>
 
 </div>
+</div>
+
+<div class="container-fluid newsletter-and-socials">
+  <div class="container d-flex justify-content-between">
+    <div>
+    <button type="button" class="btn btn-outline-primary text-light">
+        SIGN-UP NOW!
+    </button>
+    </div>
+
+  </div>
 </div>
 
 
