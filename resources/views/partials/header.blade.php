d96c393982f86da3003c74b94385f4ac4b4b12cf
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-22 01:41:22 +0100

    Feat header.blade.php - header basic structure

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,13 @@
-<header>Header</header>
+<header>
+    
+<div class="container">
+    <div class="logo"></div>
+    <nav class="menu">
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('comics') }}">Comics</a></li>
+        </ul>
+    </nav>
+    <div class="form-search"></div>
+</div>
+</header>
