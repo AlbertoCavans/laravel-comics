c6379aedb9a833412e6389915c006b19f908ea1e
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-22 08:26:38 +0100

    Feat header.blade.php - details header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,42 @@
 <header>
-    
-<div class="container">
-    <div class="logo"></div>
-    <nav class="menu">
-        <ul>
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('comics') }}">Comics</a></li>
-        </ul>
-    </nav>
-    <div class="form-search"></div>
+<div class="container-fluid towards-payments">
+  <div class="container d-flex justify-content-end">
+    <h6 class="mx-5"><a href="#">DC POWER VISA</a></h6>
+    <h6><a href="#">ADDITIONAL DC SITES</a></h6>
+  </div>
+</div>
+
+<div class="container-fluid py-2">
+  <div class="container d-flex justify-content-between align-items-center">
+      <div class="logo">
+        <img src="{{ url('dc-logo.png') }}" alt="">
+      </div>
+      <nav class="menu">
+          <ul>
+              <li><a class="h5 text-secondary" href="{{ route('home') }}">HOME</a></li>
+              <li><a class="h5 text-secondary" href="{{ route('comics') }}">COMICS</a></li>
+              <li><a class="h5 text-secondary" href="#">MOVIES</a></li>
+              <li><a class="h5 text-secondary" href="#">TV</a></li>
+              <li><a class="h5 text-secondary" href="#">GAMES</a></li>
+              <li><a class="h5 text-secondary" href="#">COLLECTIBLES</a></li>
+              <li><a class="h5 text-secondary" href="#">VIDEOS</a></li>
+              <li><a class="h5 text-secondary" href="#">FANS</a></li>
+              <li><a class="h5 text-secondary" href="#">NEWS</a></li>
+              <li><a class="h5 text-secondary" href="#">FANS</a></li>
+          </ul>
+      </nav>
+      
+      <div class="form-search">
+      <form>
+       <div class="form-group">
+         <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Search">
+       </div>
+      </div>
+    </div>
+</div>
+
+<div class="container-fluid jumbo d-flex">
+<img src="{{ url('jumbotron.jpg') }}" alt="" width="100%" class="object-fit-cover jumbo-img">
+<h3 class="label-current-series">CURRENT SERIES</h3>
 </div>
 </header>
